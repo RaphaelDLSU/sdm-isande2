f1644d0a514172cf52afb84536b9375c39437374
RaphaelDLSU <91138973+RaphaelDLSU@users.noreply.github.com>
2023-02-15 14:17:33 +0800

    comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar_top'
 import Sidebar from './components/Sidebar';
 
-
+// Homepage
 export default function App() {
   return (
     
-    <div className="App">
-      <Navbar />
-      <div className="page_body">
-        <Sidebar/>
+    <div className="App"> {/* Whole pagee */}
+      <Navbar />  {/*Navbar_top.js */}
+      <div className="page_body"> {/* Body of the page */}
+        <Sidebar/> {/*Sidebar.js */}
         
       </div>
       
