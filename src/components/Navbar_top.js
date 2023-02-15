db7c248e319f631922c9a9fc152369cd8a648485
RaphaelDLSU <91138973+RaphaelDLSU@users.noreply.github.com>
2023-02-15 17:33:50 +0800

    spdkads

--- src/components/Navbar_top.js
+++ src/components/Navbar_top.js
@@ -1,14 +1,19 @@
 import '../navbar_top.css';
+import Logo from '../logo.jpg';
+import reactLogo from '../logo.svg';
 
 
 export default function Navbar_top (){
     return (
     <nav className="nav">
         <div className='left-side-nav'>
-            
             <ul>
                 <li>
-                    <h1 className='company-title'>SDM Flow</h1>
+                    <div className='company-part'>
+                        <img src = {Logo} alt={reactLogo}></img>
+                        <h1 className='company-title'>SDM Flow</h1>
+                    </div>
+                    
                         <a>About us</a>
                         <a>Lessons</a>
                         <a>Faculty</a>
